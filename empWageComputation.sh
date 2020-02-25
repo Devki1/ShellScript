fff48659da8f788c15b9e6dc2905bad03b481517
Devki1 <gdev3195@gmail.com>
2020-02-25 14:34:33 +0530

    ADDED:Calculate the employee salary of fulltime and Parttime by using case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,33 +1,46 @@
 #!/bin/bash -x
 echo "Welcome to employee wages problem"
 isPresent=1
-parttime=1
-fulltime=2
-empperhour=20
-randomcheck=$((RANDOM%3))
+partTime=1
+fullTime=2
+empPerHour=20
+randomCheck=$((RANDOM%3))
 #To check the employee is present or not
-if(($isPresent == $randomcheck))
+if(($isPresent == $randomCheck))
 	then
 		echo "Employee is present"
 	else
 		echo "Employee is absent"
 fi
 #Calculate daily employee wage
-if(($isPresent == $randomcheck))
+if(($isPresent == $randomCheck))
 	then
 		empdayhour=8
-		salary=$(($empperhour*$empdayhour))
+		salary=$(($empPerhour*$empdayhour))
 	else
 		salary=0
 fi
 #Add part time employee and wage
-if(($parttime == $randomcheck))
+if(($partTime == $randomCheck))
 	then
 		empHrs=4
-	elif(($fulltime == $randomcheck))
+	elif(($fullTime == $randomCheck))
 	then
 		empHrs=8
 	else
 		empHrs=0
 fi
-salary=$(($empperhour*$empHrs))
+salary=$(($empPerhour*$empHrs))
+#Add parttime and fulltime employee wage by using case statement
+case $randomCheck in
+			$partTime)
+						empHrs=4
+						;;
+			$fullTime)
+						empHrs=8
+						;;
+						*)
+						empHrs=0
+						;;
+esac
+salary=$(($empPerHour*$empHrs))
